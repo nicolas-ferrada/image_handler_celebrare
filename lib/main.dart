5c9e068974474a0dd6ce0a332ab2fbeddabf78d2
nicolas-ferrada <nicofrrd@gmail.com>
2023-11-24 13:20:41 -0300

    refactor disable material3

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: const Home(),
     );
   }
 }
