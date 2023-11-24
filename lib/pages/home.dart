1fd8d237cb8016bdf012db0388acb9788ba5aec2
nicolas-ferrada <nicofrrd@gmail.com>
2023-11-24 16:22:58 -0300

    feat select new image to display in DisplayImage widget
    includes null validation
    
    home screen is now stateful widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
+
 import 'package:celebrare/styles/app_color.dart';
 import 'package:celebrare/widgets/custom_app_bar.dart';
 import 'package:celebrare/widgets/custom_container.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
+import '../widgets/display_image.dart';
 import '../widgets/home_body.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  XFile? widgetImage;
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +33,11 @@ class Home extends StatelessWidget {
               chooseFromDeviceButton(),
             ],
           ),
+          (widgetImage != null)
+              ? DisplayImage(
+                  image: File(widgetImage!.path),
+                )
+              : const SizedBox.shrink(),
         ],
       ),
     );
@@ -40,10 +56,15 @@ class Home extends StatelessWidget {
         backgroundColor: AppColor.teal,
         minimumSize: const Size(180, 40),
       ),
-      onPressed: () {
-        
-      },
+      onPressed: () async => selectNewImage(),
       child: const Text('Choose from Device'),
     );
+  }
+
+  void selectNewImage() async {
+    XFile? pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage == null) return;
+    setState(() => widgetImage = pickedImage);
   }
 }
